ae2a874b5f3e9d536d065bf332ac5c21da5c251b
AlbertMLD <albert.lungu@gmail.com>
2024-02-18 13:18:58 +0200

    Finding a record in MongoDB

--- src/com/company/MongoDB.java
+++ src/com/company/MongoDB.java
@@ -10,17 +10,21 @@ import org.bson.BsonDocument;
 import org.bson.BsonInt64;
 import org.bson.Document;
 import org.bson.conversions.Bson;
+import static com.mongodb.client.model.Filters.eq;
 
 public class MongoDB {
 
     private static MongoClient mongoClient;
+
+    private final static String TELEGRAM_DATABASE = "telegram";
+    private final static String CUSTOMERS_COLLECTION = "customers";
 
     public static void connectToDatabase() {
 
         String url = "mongodb://localhost:27017";
 
         mongoClient = MongoClients.create(url);
-        MongoDatabase mongoDatabase = mongoClient.getDatabase("telegram");
+        MongoDatabase mongoDatabase = mongoClient.getDatabase(TELEGRAM_DATABASE);
 
         try {
             Bson command = new BsonDocument("ping", new BsonInt64(1));
@@ -33,15 +37,32 @@ public class MongoDB {
 
     public static void insertNewUserID(String userID) {
 
-        MongoDatabase mongoDatabase = mongoClient.getDatabase("telegram");
-        MongoCollection<Document> customerCollection = mongoDatabase.getCollection("customers");
+        MongoDatabase mongoDatabase = mongoClient.getDatabase(TELEGRAM_DATABASE);
+        MongoCollection<Document> customerCollection = mongoDatabase.getCollection(CUSTOMERS_COLLECTION);
 
         try {
+            if (!userExists(userID)) {
+                InsertOneResult result = customerCollection.insertOne(new Document().append("_id", userID));
+                System.out.println(result.getInsertedId());
+            }
 
-            InsertOneResult result = customerCollection.insertOne(new Document().append("_id", userID));
-            System.out.println(result.getInsertedId());
         } catch (MongoException mongoException) {
             mongoException.printStackTrace();
+        }
+
+    }
+
+    public static boolean userExists(String userId) {
+
+        MongoDatabase mongoDatabase = mongoClient.getDatabase(TELEGRAM_DATABASE);
+        MongoCollection<Document> customersCollection = mongoDatabase.getCollection(CUSTOMERS_COLLECTION);
+
+        Document userDoc = customersCollection.find(eq("_id", userId)).first();
+
+        if (userDoc == null) {
+            return  false;
+        } else {
+            return true;
         }
 
     }
