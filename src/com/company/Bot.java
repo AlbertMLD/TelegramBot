b0fd91338ea8831613ced74b5996c529d88e6a2e
AlbertMLD <albert.lungu@gmail.com>
2024-04-02 19:50:52 +0300

    Update Bot.java

--- src/com/company/Bot.java
+++ src/com/company/Bot.java
@@ -2,7 +2,7 @@ package com.company;
 
 public class Bot {
 
-    static final String BOT_TOKEN = "6646245652:AAGQd-gu-pMbf3YSX97xnS_NI2dqJh8VWLE";
+    static final String BOT_TOKEN = "6646245652:AAGQd-gu-pMbf3YSX97xnS_NI2dqJh8VWLe";
     static final String USERNAME = "Java_ServiceDesk_bot";
 
 }
